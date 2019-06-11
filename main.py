08e4cea5a955b6a3a10877071c3be3945ff40e3b
Rhys Grabany <r.grabany01@qub.ac.uk>
2019-06-11 19:45:58 +0100

    Adding functionality to print to file

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import sys
 from colored import fg, bg
 
 def fileOrFolder(dataFile):
-    return fg('blue') if os.path.isdir(dataFile) else fg('green')
+
+    print(dataFile + str(os.path.isdir(dataFile)))
+
+    if (os.path.isdir(dataFile)):
+        return fg('magenta')
+    else:
+        return fg('green')
+
 
 def relative_depth(dir_path, level_offset):
     return dir_path.count(os.path.sep) - level_offset
@@ -20,33 +27,43 @@ def walklevel(some_dir, level=1):
         if num_sep + level <= num_sep_this:
             del dirs[:]
 
-def storing_folders(rootf, depthlevel):
+def storing_folders(rootf, depthlevel, output):
 
     indent = " "
 
     levelOff = rootf.count(os.path.sep) - 1
 
-    for ro, fol, fi in sorted(walklevel(rootf, depthlevel)):
+    if output is True:
+        outputFile = open("output.txt", "w")
 
-        if "." in ro:
-            continue
+
+    for ro, fol, fi in sorted(walklevel(rootf, level=depthlevel)):
 
         level = relative_depth(ro, levelOff)
 
         if level == 1:
+            
             color = fileOrFolder(ro)
             print(indent + (ro + color))
+
+            if output is True:
+                outputFile.write(indent + ro + "\n")
+            
         else:
             color = fileOrFolder(ro)
-            print(indent*level + "| " + (os.path.basename(ro) + color))
+            print(indent*level + "| " + (os.path.basename(ro)))
+
+            if output is True:
+                outputFile.write(indent*level + "| " + (os.path.basename(ro) + "\n"))
         
         level += 1
 
         for f in sorted(fi):
-            if (".") in f:
-                continue
             color = fileOrFolder(f)
             print(indent*level + "| " + os.path.basename(f) + color)
+
+            if output is True:
+                outputFile.write(indent*level + "| " + os.path.basename(f) + "\n")
         
         
     
@@ -56,5 +73,5 @@ if not len(sys.argv) > 1:
 else:
     rootf = sys.argv[1]
 
-storing_folders(rootf, 5)
+storing_folders(rootf, 5, True)
 
